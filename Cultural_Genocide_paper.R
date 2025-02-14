904a1cfad107a756b460e136e0dc5096242386dc
jschlebusch <boemeke.jan@t-online.de>
2025-02-14 21:09:49 +0100

    ADD CONTROLS

--- Cultural_Genocide_paper.R
+++ Cultural_Genocide_paper.R
@@ -21,12 +21,16 @@ df_nbp <- haven::read_dta("NBP_groups_final.dta")
 df_polity <- readxl::read_xlsx("POLITY5_annual.xlsx") %>%
   select(c(iso3c, Year, Polity2)) %>%
   rename(iso3 = iso3c)
+df_cc <- countrycode::codelist %>%
+  select(c(country.name.en, iso3c, un.region.name, un.regionintermediate.name, un.regionsub.name)) %>%
+  rename(iso3 = iso3c)
 ##------------------------------------------------------------------------------
 
 ##---- DATA PREPARATION --------------------------------------------------------
 summary(df_cg)
 summary(df_nbp)
 summary(df_polity)
+summary(df_cc)
 
 
 df_nbpcg <- df_nbp %>%
@@ -36,4 +40,64 @@ df_complete <- df_nbpcg %>%
   left_join(df_polity, by = c("iso3", "Year")) %>%
   distinct()
 
+summary(df_complete)
+
+df_complete <- df_complete %>%
+  left_join(df_cc, by = "iso3")
+
+df_complete <- df_complete %>%
+  mutate(
+    un.region.name = case_when(
+      iso3 == "CSK" ~ "Europe",
+      iso3 == "DDR"  ~ "Europe",
+      iso3 == "XKX" ~ "Europe",
+      iso3 == "YPR" ~ "Asia",
+      iso3 == "YAR" ~ "Asia",
+      iso3 == "VDR" ~ "Asia",
+      iso3 == "TWN" ~ "Asia",
+      iso3 == "SUN" ~ "Europe",
+      iso3 == "YUG" ~ "Europe",
+      TRUE ~ un.region.name
+    ),
+    un.regionsub.name = case_when(
+      iso3 == "CSK" ~ "Eastern Europe",
+      iso3 == "DDR"  ~ "Western Europe",
+      iso3 == "XKX" ~ "Southern Europe",
+      iso3 == "YPR" ~ "Western Asia",
+      iso3 == "YAR" ~ "Western Asia",
+      iso3 == "VDR" ~ "South-eastern Asia",
+      iso3 == "TWN" ~ "Eastern Asia",
+      iso3 == "SUN" ~ "Eastern Europe",
+      iso3 == "YUG" ~ "Southern Europe",
+      TRUE ~ un.regionsub.name  
+    )
+  )
+
+summary(df_complete)
+
+df_complete <- df_complete %>%
+  mutate(across(starts_with("Arrived"), ~ifelse(is.na(.), 0, .)),
+         any_cg = ifelse(is.na(any_cg), 0, any_cg))
+
+summary(as.factor(df_complete$any_cg))
+
 ##---- EDA ---------------------------------------------------------------------
+summary(as.factor(df_complete$IndigGp))
+summary(as.factor(df_complete$SpatialConc))
+
+summary(as.factor(df_complete$ViolenceAgainstGroup))
+summary(as.factor(df_complete$ViolenceDuringWar))
+print(df_complete$ViolenceDuringWar)
+summary(as.factor(df_complete$LowLevelViolence))
+print(df_complete$LowLevelViolence)
+summary(as.factor(df_complete$MassViolence))
+print(df_complete$MassViolence)
+
+summary(as.factor(df_complete$SDM))
+print(df_complete$SDM)
+
+summary(as.factor(df_complete$any_cg))
+
+summary(as.factor(df_complete$LingGpType))
+
+
